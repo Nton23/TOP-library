8ae96373db7005c7570e6d11aac07de7df0230a9
Nton23 <huutonau@gmail.com>
2023-05-21 17:37:41 -0700

    Add function to exit button

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const addBtn = document.getElementById('add-book-btn');
+const formContainer = document.getElementById('form-container');
+const blurBackground = document.getElementById('blur-background');
 addBtn.addEventListener('click', addClick);
 function addClick() {
-    const formContainer = document.getElementById('form-container');
-    const blurBackground = document.getElementById('blur-background');
     formContainer.style.display = "block"; 
     blurBackground.style.display = "block";
 }
+
+const exitBtn = document.getElementById('exit-btn');
+exitBtn.addEventListener('click', () => {
+    formContainer.style.display = "none";
+    blurBackground.style.display = "none";
+})
