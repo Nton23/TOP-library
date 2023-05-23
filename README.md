33e2d8b838924793c14364314c980dca71bb4ddb
Nton23 <huutonau@gmail.com>
2023-05-22 21:02:27 -0700

    Add array and constructor function

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # TOP-library
 -05/21/23 : -Add function to exit button from the form.
             -Add function to exit button on the book itself.
-            -
+            -Add function to checkbox on the Add form
+            -Add function to display title/author/pages/checkbox?checked
+            
--- index.js
+++ index.js
@@ -15,32 +15,60 @@ const bookAuthor = document.getElementById('book-author');
 const bookPages = document.getElementById('book-pages');
 const bookRead = document.getElementById('book-read');
 
+let titleValue;
+let authorValue;
+let pageValue;
+
+//array to store book
+let myLibrary = [];
+
+//library count
+bookCount = myLibrary.length
+
+console.log(bookCount);
+//Constructor function
+function Book(title, author, page, read) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+};
+
+//add book to array function
+function addBookToLibrary() {
+
+};
+
+//function to see if the book is read or not
+function isRead(confirmRead) {
+    let isChecked = confirmRead.checked;
+    console.log(isChecked);
+};
 
 //function to display the book form for filling out the book information
 addBtn.addEventListener('click', addClick);
 function addClick() {
-    formContainer.style.display = "block"; 
-    blurBackground.style.display = "block";
-}
+    
+    //formContainer.style.display = "block"; 
+    //blurBackground.style.display = "block";
+};
 
 //function to exit the book form
 exitBtn.addEventListener('click', () => {
     formContainer.style.display = "none";
     blurBackground.style.display = "none";
-})
+});
 
 //book remove function
 bookExitBtn.addEventListener('click', () => {
     book.remove();
-})
+});
 
 //function for submit button in the book form
 submitBtn.addEventListener('click', () => {
-    bookTitle.innerText = inputTitle.value;
-    bookAuthor.innerText = inputAuthor.value;
-    bookPages.innerText = inputPage.value;
-    formContainer.style.display = "none";
-    blurBackground.style.display = "none";
-    const isChecked = confirmRead.checked;
-    bookRead.checked = isChecked;
-})
+    isRead(confirmRead);
+    let bookCount = myLibrary.length;
+    
+});
+
+
