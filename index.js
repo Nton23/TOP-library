a1c5c69bce7327c10b011822532513d8e93d78f1
Nton23 <huutonau@gmail.com>
2023-05-23 21:02:27 -0700

    Working on a function for book structure

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ let authorValue;
 let pageValue;
 
 //array to store book
-let myLibrary = [];
+let myLibrary = [2,3];
 
-//library count
-bookCount = myLibrary.length
+//function for library count
+function bookNumber() {
+    let bookCount = myLibrary.length - 1;
+    return ++bookCount;
+}
 
-console.log(bookCount);
+
 //Constructor function
 function Book(title, author, page, read) {
     this.title = title;
@@ -45,12 +48,47 @@ function isRead(confirmRead) {
     console.log(isChecked);
 };
 
+//book structure
+function bookStructure() {
+    //Book container
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'relative w-48 h-64 shadow-2xl group';
+    containerDiv.id = 'book';
+
+    //Top container
+    const topContainer = document.createElement('div');
+    topContainer.className = 'p-4 space-y-6';
+    //title
+    const titleContainer = document.createElement('div');
+    titleContainer.className = 'space-x-3';
+    const titleSpan = document.createElement('span');
+    titleSpan.innerHTML = 'Title';
+    const titleSpanValue= document.createElement('span');
+    titleSpanValue.id = 'book-title';
+    //author
+    const authorContainer = document.createElement('div');
+    authorContainer.className = 'space-x-3';
+    const authorSpan = document.createElement('span');
+    authorSpan.innerHTML = 'Author';
+    const authorSpanValue= document.createElement('span');
+    authorSpanValue.id = 'book-author';
+    //pages
+    const pageContainer = document.createElement('div');
+    pageContainer.className = 'space-x-3';
+    const pageSpan = document.createElement('span');
+    pageSpan.innerHTML = 'Pages';
+    const pageSpanValue= document.createElement('span');
+    pageSpanValue.id = 'book-pages';
+    
+    //Bottom Container
+}
+
 //function to display the book form for filling out the book information
 addBtn.addEventListener('click', addClick);
 function addClick() {
-    
-    //formContainer.style.display = "block"; 
-    //blurBackground.style.display = "block";
+    bookStructure();
+    formContainer.style.display = "block"; 
+    blurBackground.style.display = "block";
 };
 
 //function to exit the book form
@@ -66,9 +104,5 @@ bookExitBtn.addEventListener('click', () => {
 
 //function for submit button in the book form
 submitBtn.addEventListener('click', () => {
-    isRead(confirmRead);
-    let bookCount = myLibrary.length;
-    
+    console.log(bookNumber());
 });
-
-
