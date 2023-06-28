6c2b6209ac1d27fd7d4e9089a5f9aa54904f4c7b
Nton23 <huutonau@gmail.com>
2023-06-28 14:58:44 -0700

    Add function to the submit button

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const addBtn = document.getElementById('add-btn');
 const bookDisplay = document.getElementById('book-display');
 //Variables for book container & book content
 const bookContainer = document.createElement('div');
-bookContainer.classList.add('relative', 'shadow-lg', 'h-72', 'p-5', 'group', 'hover:scale-105', 'hover:shadow-green-400');
 const topContainer = document.createElement('div');
 const topContent = document.createElement('div');
-topContent.classList.add('space-y-5', 'text-center', 'md:text-start');
 const title = document.createElement('h5');
 const titleContent = document.createElement('span');
 const author = document.createElement('h5');
@@ -14,7 +12,6 @@ const authorContent = document.createElement('span');
 const page = document.createElement('h5');
 const pageContent = document.createElement('span');
 const bottomContainer = document.createElement('div');
-bottomContainer.classList.add('absolute', 'hidden', 'group-hover:block', 'bg-slate-200', 'bottom-0', 'left-0', 'right-0', 'h-20', 'p-5')
 const bottomContent = document.createElement('div');
 const labelElement = document.createElement('label');
 labelElement.textContent = 'have you read it?'
@@ -25,8 +22,14 @@ checkboxElement.setAttribute('id', 'confirmation.checkbox')
 //Form variables
 const form = document.getElementById('form');
 const blurryBg = document.getElementById('blurry-bg');
-
-
+const submitBtn = document.getElementById('submit-btn');
+const exitBtn = document.getElementById('exit-btn');
+const formTitle = document.getElementById('form-title');
+const formAuthor = document.getElementById('form-author');
+const formPage = document.getElementById('form-page');
+let titleResult;
+let authorResult;
+let pageResult;
 
 
 //Add button event listener
@@ -46,19 +49,31 @@ function bookStructure() {
     topContent.appendChild(title);
     topContent.appendChild(author);
     topContent.appendChild(page);
+    topContent.classList.add('space-y-5', 'text-center', 'md:text-start');
     topContainer.appendChild(topContent);
     bottomContent.appendChild(labelElement);
     bottomContent.appendChild(checkboxElement);
     bottomContainer.appendChild(bottomContent);
+    bottomContainer.classList.add('absolute', 'hidden', 'group-hover:block', 'bg-slate-200', 'bottom-0', 'left-0', 'right-0', 'h-20', 'p-5');
     bookContainer.appendChild(topContainer);
     bookContainer.appendChild(bottomContainer);
+    bookContainer.classList.add('relative', 'shadow-lg', 'h-72', 'p-5', 'group', 'hover:scale-105', 'hover:shadow-green-400');
     bookDisplay.appendChild(bookContainer);
 }
 
 //Add Functions
 function addBtnFunction() {
-    form.classList.toggle('hidden');
-    form.classList.toggle('block');
-    blurryBg.classList.toggle('hidden');
-    blurryBg.classList.toggle('block');
+    form.classList.remove('hidden');
+    form.classList.add('block');
+    blurryBg.classList.remove('hidden');
+    blurryBg.classList.add('block');
 }
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    bookStructure();
+    form.classList.add('hidden');
+    form.classList.remove('block');
+    blurryBg.classList.add('hidden');
+    blurryBg.classList.remove('block');
+})
